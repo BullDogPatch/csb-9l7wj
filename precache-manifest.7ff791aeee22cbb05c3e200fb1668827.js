94501bd69bc4a361bed45be7f4dc586ee96c5b68
Code Sandbox <hello@codesandbox.io>
2022-05-30 16:56:33 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.7ff791aeee22cbb05c3e200fb1668827.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1988e6e440b7ea17d41181da9e6f08a9",
+    "url": "https://bulldogpatch.github.io/csb-9l7wj/index.html"
+  },
+  {
+    "revision": "c946c506c165e382b093",
+    "url": "https://bulldogpatch.github.io/csb-9l7wj/static/css/2.4a866b4a.chunk.css"
+  },
+  {
+    "revision": "5ea4272806fe5f87e26a",
+    "url": "https://bulldogpatch.github.io/csb-9l7wj/static/css/main.ff9e587b.chunk.css"
+  },
+  {
+    "revision": "c946c506c165e382b093",
+    "url": "https://bulldogpatch.github.io/csb-9l7wj/static/js/2.ac02b561.chunk.js"
+  },
+  {
+    "revision": "5ea4272806fe5f87e26a",
+    "url": "https://bulldogpatch.github.io/csb-9l7wj/static/js/main.67dba09e.chunk.js"
+  },
+  {
+    "revision": "9531d9cefe7f82b10b6d",
+    "url": "https://bulldogpatch.github.io/csb-9l7wj/static/js/runtime~main.82896487.js"
+  }
+]);
